c57fcc9cfb24d03a548ada6e9bee9ea880d03291
clubcaliche <cmoreira@misfirm.com>
2025-04-10 19:56:07 -0400

    Updated variable script

--- lamp/ubuntu/00-prep-vars.sh
+++ lamp/ubuntu/00-prep-vars.sh
@@ -8,9 +8,9 @@ CONF_FILE="ssl-ubuntu.conf"
 
 # Check if the file exists
 if [[ -f "$CONF_FILE" ]]; then
-    # Update the ServerName line with the host's IP address
-    sed -i "s/^ServerName .*/ServerName $HOST_IP/" "$CONF_FILE"
-    echo "Updated ServerName in $CONF_FILE to $HOST_IP"
+    # Replace ${SERVER_IP} with the host's IP address
+    sed -i "s/\${SERVER_IP}/$HOST_IP/g" "$CONF_FILE"
+    echo "Replaced \${SERVER_IP} with $HOST_IP in $CONF_FILE"
 else
     echo "Error: $CONF_FILE not found."
     exit 1
