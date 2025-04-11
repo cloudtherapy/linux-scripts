f7f43383161aa84823932be92a1b6a2c4611f7b5
Carlos Moreira <83825416+clubcaliche@users.noreply.github.com>
2025-04-11 08:59:33 -0400

    Updated to ALTER

--- lamp/ubuntu/03-secure-mysql.sh
+++ lamp/ubuntu/03-secure-mysql.sh
@@ -74,7 +74,7 @@ if is_mysql_root_password_set; then
 fi
 
 sudo mysql --user=root <<_EOF_
-  UPDATE mysql.user SET Password=PASSWORD('${db_root_password}') WHERE User='root';
+  ALTER USER 'root'@'localhost' IDENTIFIED BY '${db_root_password}';
   DELETE FROM mysql.user WHERE User='';
   DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
   DROP DATABASE IF EXISTS test;
