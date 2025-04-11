fce6f177c218b464e0fc476835f714937d097a67
Carlos Moreira <83825416+clubcaliche@users.noreply.github.com>
2025-04-11 08:21:25 -0400

    Removed touch

--- lamp/ubuntu/02-enable-ssl.sh
+++ lamp/ubuntu/02-enable-ssl.sh
@@ -1,6 +1,5 @@
 sudo a2enmod ssl
 sudo openssl req -x509 -nodes -days 730 -newkey rsa:2048 -keyout /etc/ssl/private/apache-selfsigned.key -out /etc/ssl/certs/apache-selfsigned.crt -config ubuntu-cert.conf
-sudo touch /etc/apache2/sites-available/ssl-ubuntu.conf
 sudo cp ssl-ubuntu.conf /etc/apache2/sites-available/ssl-ubuntu.conf
 sudo a2ensite ssl-ubuntu.conf
 sudo systemctl reload apache2
