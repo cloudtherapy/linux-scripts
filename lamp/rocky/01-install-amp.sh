f58b356c56f448bd044917da7e4bc5207b06e77f
Carlos Moreira <83825416+clubcaliche@users.noreply.github.com>
2025-05-28 21:50:41 -0400

    Update to httpd

--- lamp/rocky/01-install-amp.sh
+++ lamp/rocky/01-install-amp.sh
@@ -1,6 +1,6 @@
 sudo dnf upgrade -y
-sudo dnf install apache2 -y
+sudo dnf install httpd -y
 sudo dnf install mariadb-server mariadb-client -y
 sudo dnf install php -y
-sudo systemctl enable apache2
+sudo systemctl enable httpd
 sudo systemctl enable mariadb
