f9b80cd272dfaf6fddb95aca8f2ded312fab1ac8
Carlos Moreira <83825416+clubcaliche@users.noreply.github.com>
2025-05-28 21:56:27 -0400

    update apache2 to httpd

--- lamp/rocky/02-enable-ssl.sh
+++ lamp/rocky/02-enable-ssl.sh
@@ -2,4 +2,4 @@ sudo a2enmod ssl
 sudo openssl req -x509 -nodes -days 730 -newkey rsa:2048 -keyout /etc/ssl/private/apache-selfsigned.key -out /etc/ssl/certs/apache-selfsigned.crt -config rocky-cert.conf
 sudo cp ssl-rocky.conf /etc/apache2/sites-available/ssl-rocky.conf
 sudo a2ensite ssl-rocky.conf
-sudo systemctl reload apache2
+sudo systemctl reload httpd
