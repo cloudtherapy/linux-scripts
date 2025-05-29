bef55cf5bde1e4613d858cce53d5c216d38a748a
Carlos Moreira <83825416+clubcaliche@users.noreply.github.com>
2025-05-28 21:52:43 -0400

    removed mariadb-client

--- lamp/rocky/01-install-amp.sh
+++ lamp/rocky/01-install-amp.sh
@@ -1,6 +1,6 @@
 sudo dnf upgrade -y
 sudo dnf install httpd -y
-sudo dnf install mariadb-server mariadb-client -y
+sudo dnf install mariadb-server -y
 sudo dnf install php -y
 sudo systemctl enable httpd
-sudo systemctl enable mariadb
+sudo systemctl enable mariadb-server
